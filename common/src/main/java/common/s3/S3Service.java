683eeea23436eff0fc53bf7df8ddde6ac2033e37
ei-abdullah <abdullah.zafar.career@gmail.com>
2026-08-15 00:35:32 +0500

    feat(aws): added presigned url feature for generating and put object

---
+++ common/src/main/java/common/s3/S3Service.java
@@ -0,0 +1,92 @@
+package common.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.internal.sync.FileContentStreamProvider;
+import software.amazon.awssdk.http.*;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Map;
+
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final Logger logger;
+
+    public String createPresignedUrl(
+            String bucketName,
+            String keyName,
+            Map<String, String> metadata
+    ) {
+        try (S3Presigner presigner = S3Presigner.create()) {
+            PutObjectRequest putObjectRequest = PutObjectRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .key(keyName)
+                    .metadata(metadata)
+                    .build();
+
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest
+                    .builder()
+                    .putObjectRequest(putObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(10))
+                    .build();
+
+            PresignedPutObjectRequest presignedRequest = presigner
+                    .presignPutObject(presignRequest);
+
+            logger.info("Presigned URL: {}", presignedRequest.url().toString());
+            logger.info("HTTP method: {}", presignedRequest.httpRequest().method());
+
+            return presignedRequest.url().toExternalForm();
+        }
+
+    }
+
+    public void uploadFile(
+            String preSignedUrlString,
+            File file,
+            Map<String, String> metadata
+    ) {
+        try {
+            URL presignedUrl = new URI(preSignedUrlString).toURL();
+
+            SdkHttpRequest.Builder requestBuilder = SdkHttpRequest
+                    .builder()
+                    .method(SdkHttpMethod.PUT)
+                    .uri(presignedUrl.toURI());
+
+            metadata.forEach((k, v) -> requestBuilder.putHeader("x-amz-meta-" + k, v));
+
+            SdkHttpRequest request = requestBuilder.build();
+
+            HttpExecuteRequest executeRequest = HttpExecuteRequest
+                    .builder()
+                    .request(request)
+                    .contentStreamProvider(new FileContentStreamProvider(file.toPath()))
+                    .build();
+
+            try (SdkHttpClient sdkHttpClient = ApacheHttpClient.create()) {
+                HttpExecuteResponse response = sdkHttpClient
+                        .prepareRequest(executeRequest)
+                        .call();
+                logger.info("Response code: {}", response.httpResponse().statusCode());
+            }
+        } catch (IOException | URISyntaxException e) {
+            logger.error("Error uploading file: {}", e.getMessage());
+        }
+    }
+}
