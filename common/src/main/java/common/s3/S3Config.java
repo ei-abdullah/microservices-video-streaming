bb9b3c794e8e758cb9788fe44eaf901800baee02
ei-abdullah <abdullah.zafar.career@gmail.com>
2026-08-15 00:35:32 +0500

    feat(aws): added presigned url feature for generating and put object

--- common/src/main/java/common/s3/S3Config.java
+++ common/src/main/java/common/s3/S3Config.java
@@ -1,4 +1,4 @@
-package common.config;
+package common.s3;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -6,16 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 
 @Configuration
-public class AwsS3Config {
+public class S3Config {
+
+    @Value("${aws.region}")
+    private String region;
 
     @Bean
-    public S3Client s3Client(
-            @Value("${aws.region}") String region
-    ) {
+    public S3Client s3Client() {
         return S3Client
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner
                 .builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.builder().build())
