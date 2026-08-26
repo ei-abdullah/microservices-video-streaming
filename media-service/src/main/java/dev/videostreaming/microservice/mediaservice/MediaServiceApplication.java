e91cb48be58c593d135be9f73aa9017ce858c9ed
ei-abdullah <abdullah.zafar.career@gmail.com>
2026-08-27 00:01:16 +0500

    feat(encoding): added ffmpeg encoding to transcoding-service

--- media-service/src/main/java/dev/videostreaming/microservice/mediaservice/MediaServiceApplication.java
+++ media-service/src/main/java/dev/videostreaming/microservice/mediaservice/MediaServiceApplication.java
@@ -1,9 +1,6 @@
 package dev.videostreaming.microservice.mediaservice;
 
-import common.config.AwsS3AutoConfiguration;
-import common.config.CommonWebAutoConfiguration;
-import common.config.JwtSecurityAutoConfiguration;
-import common.config.UtilsConfig;
+import common.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,7 +9,13 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-@Import({CommonWebAutoConfiguration.class, AwsS3AutoConfiguration.class, JwtSecurityAutoConfiguration.class, UtilsConfig.class})
+@Import({
+		CommonWebAutoConfiguration.class,
+		AwsS3AutoConfiguration.class,
+		JwtSecurityAutoConfiguration.class,
+		RabbitMqConfig.class,
+		UtilsConfig.class
+})
 public class MediaServiceApplication {
 
 	public static void main(String[] args) {
