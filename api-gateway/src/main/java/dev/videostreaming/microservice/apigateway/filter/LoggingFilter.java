6a20c043e398079f4874b5534130ebaaa31d9d78
ei-abdullah <abdullah.zafar.career@gmail.com>
2026-09-15 19:20:52 +0500

    feat(gateway): injected logger and exposed health routes

--- api-gateway/src/main/java/dev/videostreaming/microservice/apigateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/dev/videostreaming/microservice/apigateway/filter/LoggingFilter.java
@@ -1,6 +1,7 @@
 package dev.videostreaming.microservice.apigateway.filter;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -11,8 +12,10 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-@Slf4j
 public class LoggingFilter implements GlobalFilter, Ordered {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
